306eaa11f1cfdde49453553905cc4556f4f280cf
Rui Marques <a.marques1967@gmail.com>
2025-10-09 23:36:27 +0100

    feat: change the status so when redirect after post uses the right status code

--- Core/Helpers/Http.php
+++ Core/Helpers/Http.php
@@ -1,7 +1,13 @@
 <?php
 
-function redirect(string $url): void
+declare(strict_types=1);
+
+function redirect(string $url, int $status = 302): void
 {
-    header("Location: $url");
+    if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST' && $status === 302) {
+        $status = 303;
+    }
+
+    header("Location: {$url}", true, $status);
     exit();
 }
